04f7b22567eff694a723a8e6e136099dbae29c87
raselpy <rasel.py@gmail.com>
2019-01-10 21:57:27 +0600

    Update Question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -64,7 +64,8 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+        return response('Updated', Response::HTTP_ACCEPTED);
     }
 
     /**

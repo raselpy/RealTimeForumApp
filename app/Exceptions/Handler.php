3af38b7cfbb5a12e0b15f2a837f76e105b58d67b
raselpy <rasel.py@gmail.com>
2019-01-19 12:08:00 +0600

    create user signup & JWT middleware and Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Exception;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -46,6 +48,16 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof TokenBlacklistedException) {
+            return response(['error' => 'Token can`t be used,get new one', 400]);
+        } 
+        else if ($exception instanceof TokenInvalidException) {
+            return response(['error' => 'Token is not Invalid', 400]);
+        }
+        else if ($exception instanceof JWTException) {
+            return response(['error' => 'Token is not provided', 400]);
+        }
+       
         return parent::render($request, $exception);
     }
 }
--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class ReplyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('JWT', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *

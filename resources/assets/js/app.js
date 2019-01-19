b2f330cac09325eac0de04ab25ffc82f6ee6824a
raselpy <rasel.py@gmail.com>
2019-01-19 12:45:50 +0600

    install & setup vuetify

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to

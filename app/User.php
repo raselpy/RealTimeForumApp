b4e36775f13f4106497744d5ec254d63b4f95c64
raselpy <rasel.py@gmail.com>
2019-01-19 11:02:32 +0600

    implement JWT auth

--- app/User.php
+++ app/User.php
@@ -2,10 +2,12 @@
 
 namespace App;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Model\Question;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -30,4 +32,25 @@ class User extends Authenticatable
     public function question(){
         return $this->hasMany(Question::class);
     }
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,19 @@ use Illuminate\Http\Request;
 */
 
 Route::apiResource('/question','QuestionController');
+Route::apiResource('/category', 'CategoryController');
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
